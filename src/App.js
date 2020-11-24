93b14211e2ce02e0d4d406c5afd1eea80aedff06
ahmed devcoder <ahmed.development.engineer@outlook.com>
2020-11-24 21:55:12 +0100

    update JSON

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import ErrorPage from './Componenets/ErrorPage';
 import Landing from './Componenets/Landing';
 import Login from './Componenets/Login';
