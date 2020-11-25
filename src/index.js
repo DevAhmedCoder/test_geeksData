725a00b577e9b9c86ee5b17bd32705366648c27d
ahmed devcoder <ahmed.development.engineer@outlook.com>
2020-11-25 22:11:16 +0100

    Update style

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import "./index.css";
 import Firebase, { FirebaseContext } from './Componenets/Firebase';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
