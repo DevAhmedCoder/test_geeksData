be758a2f42677af8524df2f1dfe95ae8614c8c4e
ahmed devcoder <ahmed.development.engineer@outlook.com>
2020-11-25 13:20:17 +0100

    build a landing and finished style

--- src/Componenets/Landing/description.js
+++ src/Componenets/Landing/description.js
@@ -2,6 +2,7 @@ import React, { useState, useContext, useEffect } from 'react'
 import { Link } from 'react-router-dom';
 import Logout from '../Logout';
 import { FirebaseContext } from '../Firebase';
+import './index.css';
 
 
 
@@ -32,20 +33,35 @@ const Landing = (props) => {
                     <Link className="text-white text-decoration-none" to="/landing">About</Link>
                     <Logout />
                 </nav>
-                <div>
-                    Description
-                    <p>defefdefdf</p>
-                    <p>defefdefdf</p>
-                    <p>defefdefdf</p>
-                    <p>defefdefdf</p>
-                    <p>defefdefdf</p>
-                    <p>defefdefdf</p>
-                    <p>defefdefdf</p>
+                <div className="container description">
+                    <div className="row justify-content-md-cente text-center ">
+                        <div className="col col-lg-4">
+                            <h3>Declarative</h3>
+                            <p>
+                                Component-Based
+                                Build encapsulated components that manage their own state, then compose them to make complex UIs.
 
+                                Since component logic is written in JavaScript instead of templates, you can easily pass rich data through your app and keep state out of the DOM.
+
+                                Learn Once, Write Anywhere
+                            </p>
+                        </div>
+                        <div className="col col-lg-4">
+                            <h3>Component-Based</h3>
+                            <p>
+                                Build encapsulated components that manage their own state, then compose them to make complex UIs.
+
+                                Since component logic is written in JavaScript instead of templates, you can easily pass rich data through your app and keep state out of the DOM.
+                            </p>  </div>
+                        <div className="col col-lg-4"> <h3>Learn Once, Write Anywhere</h3>
+                            <p>
+                                We don’t make assumptions about the rest of your technology stack, so you can develop new features in React without rewriting existing code.
+
+                                React can also render on the server using Node and power mobile apps using React Native.
+                            </p>  </div>
+                    </div>
                 </div>
             </div>
         );
 }
 export default Landing;
-
-{/*  */ }
--- src/Componenets/Landing/index.js
+++ src/Componenets/Landing/index.js
@@ -2,6 +2,8 @@ import React, { useState, useContext, useEffect } from 'react'
 import { Link } from 'react-router-dom';
 import Logout from '../Logout';
 import { FirebaseContext } from '../Firebase';
+import './index.css';
+import logo from './logo192.png';
 
 
 
@@ -27,23 +29,23 @@ const Landing = (props) => {
         <p>Please whait...</p>
     ) :
         (
-<div>
-             <nav id="navbar" className=" fixed-top navbar navbar-dark bg-dark d-flex justify-content-around">
+            <div>
+                <nav id="navbar" className=" fixed-top navbar navbar-dark bg-dark d-flex justify-content-around">
                     <Link className="text-white text-decoration-none" to="/landing/description">Description</Link>
                     <Logout />
                 </nav>
-            <div  className="container-fluid border about ">
-                <div  className="row ">
-                    <div  className="col align-self-center ">
-                        <h1>About</h1>
-                        <h1>About</h1>
-                        <h1>About</h1>
-                        <h1>About</h1>
-                        <h1>About</h1>
+                <div className="container-fluid border about">
+                    <div className="row ">
+                        <div className="col justify-content-md-center ">
+                            <img src={logo} alt="Logo" />
+                            <h1>React JS</h1>
+                            <br/>
+                            <p className="w-50 col-md-auto m-auto" >React makes it painless to create interactive UIs. Design simple views for each state in your application, and React will efficiently update and render just the right components when your data changes.<br/>
+Declarative views make your code more predictable and easier to debug.</p>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
         );
 }
 export default Landing;
